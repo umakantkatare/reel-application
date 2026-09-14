e6c48206bc36b5b6e5f7568125b505e221002cd7
umakant katare <umakantkatare11@gmail.com>
2026-09-14 15:25:09 +0530

    update imagekit-auth route

--- src/app/api/upload-auth/route.ts
+++ src/app/api/upload-auth/route.ts
@@ -1,12 +1,22 @@
-import { getUploadAuthParams } from "@imagekit/next/server"
+import { getUploadAuthParams } from "@imagekit/next/server";
 
 export async function GET() {
+  try {
+    const ImagekitAuthParams = getUploadAuthParams({
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY as string,
+    });
 
-    const { token, expire, signature } = getUploadAuthParams({
-        privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string, 
-        publicKey: process.env.IMAGEKIT_PUBLIC_KEY as string,
-       
-    })
-
-    return Response.json({ token, expire, signature, publicKey: process.env.IMAGEKIT_PUBLIC_KEY })
+    return Response.json({
+      ImagekitAuthParams,
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+    });
+  } catch (error) {
+    return Response.json(
+      {
+        error: "Authentication for Imagekit failed",
+      },
+      { status: 500 },
+    );
+  }
 }
